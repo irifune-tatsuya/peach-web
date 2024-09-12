38ab00f803aed4fced3131a3789e83c17f662234
Tatsuya Irifune <irifune.tatsuya@gmail.com>
2024-09-12 23:02:35 +0900

    add_pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,66 @@ import Footer from '@/components/Footer';
 import './globals.css';
 import styles from './layout.module.css';
 import { Provider } from './providers/chakra-ui/Provider';
-import { NextPage } from 'next';
+import { Metadata, NextPage } from 'next';
 
-export const metadata = {
+const siteName = 'ピーチウェブ -岡山のWEBブランディングサービス-';
+const description =
+  'あなたの仕事が永く愛されますように。お客様の事業の魅力を最大限発信するWEBブランディングをご提案する岡山の会社ピーチウェブの公式サイトです。ホームページの制作・運用、事業計画の見直しなど多岐にわたってサポートいたします。';
+
+export const metadata: Metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
-  title: 'Simple Blog',
-  description: 'A simple blog presented by microCMS',
+  title: {
+    template: `%s | ${siteName}`,
+    default: `TOP | ${siteName}`,
+  },
+  description: description,
   openGraph: {
-    title: 'Simple Blog',
-    description: 'A simple blog presented by microCMS',
-    images: '/ogp.png',
+    title: siteName,
+    description: description,
+    url: '/',
+    siteName: siteName,
+    images: [
+      {
+        url: `/ogp.jpg`,
+        width: 800,
+        height: 600,
+      },
+      {
+        url: `ogp-alt.jpg`,
+        width: 1800,
+        height: 1600,
+        alt: siteName,
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'article',
   },
-  alternates: {
-    canonical: '/',
+  icons: {
+    icon: `/favicon.ico`,
+    apple: `/apple-touch-icon.png`,
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description: description,
+    creator: '@irifune333',
+    images: {
+      url: `og.jpg`,
+      alt: siteName,
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  manifest: '/manifest.json',
 };
 
 type Props = {
--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrinctMode: true,
+});
