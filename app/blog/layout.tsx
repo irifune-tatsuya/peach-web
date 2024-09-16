1a7f5b3079f310600d2cc530c9c5c8a5673e8ff2
Tatsuya Irifune <irifune.tatsuya@gmail.com>
2024-09-16 12:55:51 +0900

    add comments

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,10 +1,10 @@
-import { getTagList } from '@/libs/microcms';
-import { LIMIT } from '@/constants';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
-import Nav from '@/components/Nav';
+// import { getTagList } from '@/libs/microcms';
+// import { LIMIT } from '@/constants';
+// import { Header } from '@/components/Header';
+// import { Footer } from '@/components/Footer';
+// import Nav from '@/components/Nav';
 // import './globals.css';
-import styles from './layout.module.css';
+// import styles from './layout.module.css';
 
 // export const metadata = {
 //   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -20,22 +20,22 @@ import styles from './layout.module.css';
 //   },
 // };
 
-type Props = {
-  children: React.ReactNode;
-};
+// type Props = {
+//   children: React.ReactNode;
+// };
 
-export default async function RootLayout({ children }: Props) {
-  const tags = await getTagList({
-    limit: LIMIT,
-  });
-  return (
-    <html lang="ja">
-      <body>
-        <Header />
-        <Nav tags={tags.contents} />
-        <main className={styles.main}>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
-}
+// export default async function RootLayout({ children }: Props) {
+//   const tags = await getTagList({
+//     limit: LIMIT,
+//   });
+//   return (
+//     <html lang="ja">
+//       <body>
+//         <Header />
+//         <Nav tags={tags.contents} />
+//         <main className={styles.main}>{children}</main>
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
