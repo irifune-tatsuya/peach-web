e78b2a6f7cbfb55eb2bf0c27b1ea262ba0d84b59
Tatsuya Irifune <irifune.tatsuya@gmail.com>
2025-10-13 17:11:56 +0900

    feat(ui): Refactor loading screen and enhance article slider

--- src/components/features/LatestArticleList/index.tsx
+++ src/components/features/LatestArticleList/index.tsx
@@ -2,12 +2,13 @@
 
 import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import { Article } from '@/lib/microcms';
 import { LandscapeCard } from '@/components/ui/LandscapeCard';
 import { PortraitCard } from '@/components/ui/PortraitCard';
-
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
 type Props = {
   articles: Article[];
@@ -20,27 +21,42 @@ export const LatestArticleList: FC<Props> = ({ articles, category }) => {
   }
 
   return (
-    <Swiper
-      modules={[Autoplay]}
-      loop={true}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-      }}
-      slidesPerView={'auto'}
-      spaceBetween={16}
-      centerInsufficientSlides={true}
-      className="!py-2 !px-1"
-    >
-      {articles.map((article) => (
-        <SwiperSlide key={article.id} className="!w-auto transition-opacity hover:opacity-80">
-          {category === 'article' ? (
-            <LandscapeCard article={article} category={category} />
-          ) : (
-            <PortraitCard article={article} category={category} />
-          )}
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative group">
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        navigation={{
+          nextEl: '.latest-swiper-button-next',
+          prevEl: '.latest-swiper-button-prev',
+        }}
+        slidesPerView={'auto'}
+        spaceBetween={16}
+        centerInsufficientSlides={true}
+        className="!py-2 !px-1"
+      >
+        {articles.map((article) => (
+          <SwiperSlide
+            key={article.id}
+            className="!w-[80vw] md:!w-100 transition-opacity hover:opacity-80"
+          >
+            {category === 'article' ? (
+              <LandscapeCard article={article} category={category} />
+            ) : (
+              <PortraitCard article={article} category={category} />
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button className="latest-swiper-button-prev absolute top-1/2 -translate-y-1/2 left-4 z-10 p-2 bg-white/80 rounded-full shadow-md transition-opacity duration-300 hidden md:flex group-hover:opacity-100 opacity-0">
+        <ChevronLeft className="w-6 h-6 text-gray-700" />
+      </button>
+      <button className="latest-swiper-button-next absolute top-1/2 -translate-y-1/2 right-4 z-10 p-2 bg-white/80 rounded-full shadow-md transition-opacity duration-300 hidden md:flex group-hover:opacity-100 opacity-0">
+        <ChevronRight className="w-6 h-6 text-gray-700" />
+      </button>
+    </div>
   );
 };
